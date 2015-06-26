91db2f8311697a09b2f51f6d2732a48c728c01b6
James Carlson <jamescarlson@Jamess-MacBook-Pro.local>
2015-06-25 18:42:25 -0600

    initial Commit

--- FavoriteThings-Objc/AppDelegate.m
+++ FavoriteThings-Objc/AppDelegate.m
@@ -15,11 +15,14 @@
 @implementation AppDelegate
             
 
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    
     return YES;
 }
 
